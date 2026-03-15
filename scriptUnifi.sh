364bf88dca0bb144ccfc019b6d1d17ad148e5f43
unknown <soubringel@gmail.com>
2026-03-15 15:45:49 -0300

    Script basico para unifi reboot

---
+++ scriptUnifi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+source /home/unifi/.env
+
+USER_SSH="$USER"
+PASS_SSH="$PASS"
+
+
+IPS=(
+192.168.0.x
+192.168.0.x
+192.168.1.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.3.x
+192.168.1.x
+192.168.0.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+192.168.2.x
+)
+# valide se está ativo o ssh nas configuras da unifi
+# pegue o usuário e senha em settings/system/controller e veja a opção - Device SSH Authentication
+# Você precisa isntalar o sshpass
+# valide se suas antenas tem algum update ou esta tendo comunicação
+for IP in "${IPS[@]}"
+do
+        if sshpass -p "$PASS_SSH" ssh -o StrictHostKeyChecking=no $USER_SSH@$IP "reboot";then
+                echo "Reiniciando... Antena $IP"
+        else
+                echo "Falha ao reiniar Antena: $IP"
+                echo "passando para proxima"
+        fi
+done
+
+## script simples mas que funcionou aqui na empresa
+# validado pelo analista 
